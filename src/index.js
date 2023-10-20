80c8984bcc4a6f975e04656c2cca7cb439eada4b
axelrivero <ing.axelr@gmail.com>
2023-10-20 09:58:43 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Greeting() {
+  function add(x, y) {
+    return x + y;
+  }
+
+  return <h1>{add(10, 30)}</h1>;
+}
+
+root.render(
+  <>
+    <Greeting />
+    <Greeting />
+    <Greeting />
+    <Greeting />
+  </>
+);
